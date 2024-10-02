bf527d5940286d64e1fb0dc4a06bc7aa8d6f76f6
Prashant-Borde <bordeprashant12@gmail.com>
2024-10-02 20:35:49 +0530

    update

--- main2.py
+++ main2.py
@@ -12,6 +12,7 @@ from flask_caching import Cache
 import requests
 from bs4 import BeautifulSoup
 
+
 # Initialize Flask app
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a strong secret key
